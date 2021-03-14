f6be4fafd0890d76ee912b481984719bdd8d0b6d
Daria Omelkina <odasha2001@gmail.com>
2021-03-14 04:03:08 +0200

    Fixed yews whims.

--- website/site-wasm/src/lib.rs
+++ website/site-wasm/src/lib.rs
@@ -1,3 +1,4 @@
+#![recursion_limit="1024"]
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
@@ -36,11 +37,116 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div class="section">
-                <p class="subtitle">{"Library"}</p>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <body>
+            <header>
+                <div class="section">
+                    <h1 class="title">{"Mini Google"}</h1>
+                    <h2 class="subtitle">{"Experimental Search Engine"}</h2>
+                </div>
+            </header>
+
+            <section class="section">
+                <div class="columns">
+                    <div class="column is-one-quarter">
+                        <h2 class="subtitle">{"Library"}</h2>
+                        <ul>
+                            <li><a href="">
+                                <div class="columns">
+                                    <div class="column is-1">
+                                        <i class="fa fa-eye">
+                                        </i>
+                                    </div>
+                                    <div class="column">
+                                        {"Search History"}
+                                    </div>
+                                </div>
+                            </a></li>
+                            <li><a href="">
+                                <div class="columns">
+                                    <div class="column is-1">
+                                        <i class="fa fa-bookmark">
+                                        </i>
+                                    </div>
+                                    <div class="column">
+                                        {"Bookmarks"}
+                                    </div>
+                                </div>
+                            </a></li>
+                        </ul>
+                    </div>
+                    <div class="column">
+                        <div class="container">
+                            <div class="field">
+                                <div class="columns">
+                                    <div class="column is-1">
+                        <span class="icon is-large">
+                            <i class="fa fa-meh-o"></i>
+                        </span>
+                                    </div>
+                                    <div class="column">
+                                        <form action="http://localhost:5001" method="post">
+                                            <div class="field has-addons">
+                                                <p class="control is-expanded">
+                                                    <input class="input is-link" type="text"
+                                                    placeholder="Search" name="user_search"></input>
+                                                </p>
+                                                <div class="control">
+                                                    <button type="submit" class="button is-link">
+                                                        <i class="fa fa-search"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="column is-3">
+                                        <div class="dropdown is-hoverable">
+                                            <div class="dropdown-trigger">
+                                                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                                                    <span>{"Search Language"}</span>
+                                                    <span class="icon is-small">
+                                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                                                </button>
+                                            </div>
+                                            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                                                <div class="dropdown-content">
+                                                    <a href="#" class="dropdown-item is-active">
+                                                        {"English"}
+                                                    </a>
+                                                    <a class="dropdown-item">
+                                                        {"Українська"}
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <ul class="nostyle home">
+                                            <li class="section">
+                                                <div class="card">
+                                                    <a href="{{ page.permalink | safe }}">
+                                                        <div class="card-content">
+                                                            <h2 class="title is-4"> {"Title"}</h2>
+                                                            <p class="subtitle">{"Path"}</p>
+                                                            <p class="subtitle">{"Description"}</p>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                            </li>
+                                    <div style="text-align: center">
+                                        <h2>
+                                            <span class="inactive"><i class="fa fa-arrow-left"></i></span>
+                                            {"1 page"}
+                                                <a class="active" href="{{ paginator.next }}"><i class="fa fa-arrow-right"></i></a>
+                                        </h2>
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            </body>
         }
     }
 }
