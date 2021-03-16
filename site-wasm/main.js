9dfb416afb6040b921b3d78e42b0c57049626ffa
Daria Omelkina <odasha2001@gmail.com>
2021-03-16 16:21:35 +0200

    Attemted to send a request using rocket crate.

--- site-wasm/main.js
+++
@@ -1,6 +0,0 @@
-import init, { run_app } from "./pkg/site_wasm.js";
-async function main() {
-  await init("/pkg/site_wasm_bg.wasm");
-  run_app();
-}
-main();
--- website/src/main.rs
+++ website/src/main.rs
@@ -1,12 +1,18 @@
 #![feature(decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket::Request;
 use rocket::response::content::Json;
-// use rocket::request::Form;
+use rocket::request::{self, FromRequest, Request};
+use rocket::request::Form;
 use rocket_contrib::templates::Template;
-use serde::Serialize;
+use std::collections::HashMap;
+use rocket::http::{Cookie, CookieJar};
+// use async_trait::async_trait;
+
+// use serde::Serialize;
 
 // #[derive(FromForm, Debug)]
 // struct Book {
@@ -15,25 +21,51 @@ use serde::Serialize;
 //   isbn: String
 // }
 
+// #[derive(FromForm, Debug)]
+// struct SearchRequest {
+//     title: String,
+//     // url: String,
+//     // description: String
+// }
+
+#[derive(Debug)]
+struct SearchRequest(String);
+
+// #[rocket::async_trait]
+impl<'a,'r> FromRequest<'a,'r> for SearchRequest {
+    type Error = std::convert::Infallible;
+
+    //TODO: async
+    fn from_request(request: &'r Request<'_>) -> request::Outcome<SearchRequest, Self::Error> {
+        request.cookies()
+            .get_private("user_search")
+            .and_then(|cookie| cookie.value().parse().ok())
+            .map(|id| SearchRequest(id))
+            .or_forward(())
+    }
+}
+
 #[get("/")]
-fn index() -> Template {
-  #[derive(Serialize)]
-  struct Context {
-    first_name: String,
-    last_name: String
-  }
+fn index(search_request: SearchRequest) -> Template {
+    let mut context = HashMap::new();
+    context.insert("title", search_request.0);
+    // #[derive(Serialize)]
+    // struct Context {
+    //   first_name: String,
+    //   last_name: String
+    // }
+    //
+    // let context = Context {
+    //   first_name: String::from("Jane"),
+    //   last_name: String::from("Doe")
+    // };
 
-  let context = Context {
-    first_name: String::from("Jane"),
-    last_name: String::from("Doe")
-  };
-
-  Template::render("home", context)
+    Template::render("home", &context)
 }
 
 #[get("/hello")]
 fn hello() -> Json<&'static str> {
-  Json("{
+    Json("{
     'status': 'success',
     'message': 'Hello API!'
   }")
@@ -54,10 +86,10 @@ fn not_found(_req: &Request) -> String {
 // }
 
 fn main() {
-  rocket::ignite()
-    .register(catchers![not_found])
-    .mount("/", routes![index])
-    .mount("/api", routes![hello])
-    .attach(Template::fairing())
-    .launch();
+    rocket::ignite()
+        .register(catchers![not_found])
+        .mount("/", routes![index])
+        .mount("/api", routes![hello])
+        .attach(Template::fairing())
+        .launch();
 }
