0d3e5d3d299c118622fb4bc05994e810a4ce80ad
Daria Omelkina <odasha2001@gmail.com>
2021-03-22 23:50:55 +0200

    Added structure for search results, added results to the webapp.

--- website/src/main.rs
+++ website/src/main.rs
@@ -8,14 +8,21 @@ use rocket::request::Request;
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 use rocket_contrib::serve::StaticFiles;
+use serde::Serialize;
+// use tera::Context;
 // use rocket::response::content::Json;
+
+#[derive(Serialize)]
+struct Result {
+    title: String,
+    path: String,
+    description: String,
+}
 
 // Homepage of the website
 #[get("/")]
 fn index() -> Template {
-    let mut context = HashMap::new();
-    context.insert("title", String::from("Jane"));
-
+    let context: HashMap<&str, String> = HashMap::new();
     Template::render("home", &context)
 }
 
@@ -24,9 +31,28 @@ fn index() -> Template {
 #[get("/search?<user_search>")]
 fn search_page(user_search: &RawStr) -> Template {
     let mut context = HashMap::new();
-    context.insert("title", String::from(user_search.url_decode().unwrap()));
+    let result1 = Result {
+        title: String::from(String::from(user_search.url_decode().unwrap())+" one"),
+        path: String::from("one path"),
+        description: String::from("one description"),
+    };
+    let result2 = Result {
+        title: String::from(String::from(user_search.url_decode().unwrap())+" two"),
+        path: String::from("two path"),
+        description: String::from("two description"),
+    };let result3 = Result {
+        title: String::from(String::from(user_search.url_decode().unwrap())+" three"),
+        path: String::from("three path"),
+        description: String::from("three description"),
+    };
 
-    Template::render("home", &context)
+    let results = [result1, result2, result3];
+    // context.insert("title", String::from(user_search.url_decode().unwrap()));
+    // let mut context = Context::new();
+    context.insert("results", &results);
+    // tera.render("products/product.html", &context)?;
+
+    Template::render("search", &context)
 }
 
 // Catching some errors that might occur
