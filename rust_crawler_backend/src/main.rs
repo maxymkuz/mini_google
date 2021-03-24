cc8ebaef6fab559703c899f74f2214a426ed6cc2
maxymkuz <maxymkuz@gmail.com>
2021-03-24 21:04:37 +0200

    Added json serialization functionality to parser. Ready to push to db now.

--- rust_crawler_backend/src/main.rs
+++ rust_crawler_backend/src/main.rs
@@ -1,37 +1,67 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-// use std::intrinsics::prefetch_read_instruction;
+use serde_json::json;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
 // Module that currently is supposed to read data from file, and push it to database somehow (NOT IMPLEMENTED YET)
 // Later, this will be fully-functional backend for crawlers to identify the language and talk to db
 
-mod lib;
+mod lib; // MODULE THAT TALKS TO PYTHON LANGDETECT
+
+// Struct to better represent a single Website as json. It has a primitive for now.
+// FOR FUTURE: add vector of websites, vector of different languages?, and their probabilities
+#[derive(Serialize, Deserialize)]
+struct Website {
+    url: String,
+    text: String,
+    language: String,
+}
+
 
 fn main() {
     file_to_db();
 }
 
-/// Example usage of the query sender
+// Function that parses file line by line, and adds url and text to database(DB NOT IMPLEMENTED YET)
 #[tokio::main]
 async fn file_to_db() {
-    println!("got here");
+
+    // Reading from pre-made file with data line by line
     if let Ok(lines) = read_lines("./data/100_lines_collected_data.txt") {
+        let mut website: Website = Website{
+            url: String::from("sample data"),
+            text: String::from("sample data"),
+            language: String::from("sample data"),
+        };
+
         for (index, line) in lines.enumerate() {
             if let Ok(ip) = line {
-                if index % 3 == 0 { // If it is a sequence of links, divided by space
-                    println!("{}", ip);
+                if index % 3 == 0 { // Parsing a vector of links, divided by space
+                    // First link=this page url. All subsequent
                     let link_vector: Vec<&str> = ip.split(' ').collect::<Vec<&str>>(); // we don't need any memory efficiency here, it is all temporary
 
-                    println!("{} {:?}", index, link_vector);
-                    // Тут треба буде повставляти в базу
+                    // Getting only the link of the page, for now
+                    website.url = link_vector[0].to_owned(); // cloning data?
                 }
-                if index % 3 == 1 { // if it is just a text
-                    let text: String = ip;
-                    println!("{}", text);
-                    let languages = lib::send_lang_detection_query(&text).await.unwrap();
-                    // languages[0] = ("en".parse().unwrap(), 0.0001);
-                    let dominant_lang = &languages[0];
-                    println!("{:?}", dominant_lang);
+                if index % 3 == 1 { // if it is a website text
+                    website.text = ip;
+
+                    let languages :Vec<(String, f64)> = lib::send_lang_detection_query(&website.text).await.unwrap();
+                    // saving only a dominant language(with the highest probability) to struct
+                    website.language = languages[0].0.to_owned();
+
+                    // Creating a json to push to database
+                    let website_json = json!({
+                        "url": website.url,
+                        "text": website.text,
+                        "language": website.language
+                    });
+
+                    println!("{}", website_json.to_string());
+
+                    // HERE, WE ARE READY TO SEND THIS JSON TO DB
                 }
             }
         }
@@ -45,43 +75,3 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
-
-
-
-
-
-
-// fn change_and_get_first_element(a: &mut Vec<i32>) -> i32 {
-//     a[0] = 4;
-//     a[0]
-// }
-
-
-// fn plus_one(a: i32) -> i32 {
-//     a + 1
-// }
-//
-// fn testing() {
-//     let mut c: [i32; 3] = [1, 2, 3];
-//     c[0] = 102;
-//     // let e = ["x"; 5]; // ["x", "x", "x", "x", "x"]
-//     let square = |i| 3 * i * i; // { } are optional for single-lined closures
-//     println!("{} {}", square(2), c[0]);
-//     println!("{:#?}", c);
-//     // let is_below_eighteen = if age < 18 { true } else { false }; // true
-//
-//     let mut v: Vec<i64> = vec![1, 2, 3];
-//
-//     for i in &v {
-//         println!("A reference to {}", i);
-//     }
-//
-//     for i in &mut v {
-//         println!("A mutable reference to {}", i);
-//     }
-//
-//     for i in v {
-//         println!("Take ownership of the vector and its element {}", i);
-//     }
-// }
