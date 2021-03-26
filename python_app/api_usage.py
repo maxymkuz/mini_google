1363c82b71d0930db8c18213085637f54aa82b04
maxymkuz <maxymkuz@gmail.com>
2021-03-26 21:05:40 +0200

    Finally, stable connection to elastic for Andriy. Added another python container.

--- python_app/api_usage.py
+++ python_app/api_usage.py
@@ -1,15 +1,25 @@
-import requests
 import time
-time.sleep(1) # just in case, it's an example, not a production app
+import requests
+
+print("hey there 1\n\n\n")
+
+time.sleep(10)  # just in case, it's an example, not a production app
 
 
-text_to_detect = "Ich heise Hello!, Dungeon Master is dominating"
+print("hey there2\n\n\n")
+r = requests.post("http://elasticsearch:9200", data={"text": "query"})
+print(r)
 
-res = requests.post('http://lang_detect_python:5001/detect', json={"text":text_to_detect})
 
-if res.ok:
-    print(res.json()["response"])
-else: 
-    # If the input is wrong, or Internal error has occured:
-    print("Sth unexpected has happened, handle it")
-    print(res.json()["response"])
+
+#
+# text_to_detect = "Ich heise Hello!, Dungeon Master is dominating"
+#
+# res = requests.post('http://lang_detect_python:5001/detect', json={"text":text_to_detect})
+#
+# if res.ok:
+#     print(res.json()["response"])
+# else:
+#     # If the input is wrong, or Internal error has occured:
+#     print("Sth unexpected has happened, handle it")
+#     print(res.json()["response"])
